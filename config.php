7393b5c8c9b512f41a3f7b1a52689e036e9934aa
Vincent <vincent@eazymatch.nl>
2012-06-20 16:54:09 +0200

    removed stint instance

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 	return  array(
 		// slug name of eazymatch instance 
-		'instance'	=> 'stint-multiservices',
+		'instance'	=> 'instantienaam',
 		
 		// public eazymatch key
-		'key'		=> 'stint-multiservices-rWRQAcQ4Vh',
+		'key'		=> 'your-private-key',
 		
 		// private secret
-		'secret'	=> 'E99E-896A-DFC5-A2E0-8C70-074A',
+		'secret'	=> 'your-private-secret',
 		
 		// debug mode ?
-		'debug'		=> 'false',
+		'debug'		=> 'true',
 	);
